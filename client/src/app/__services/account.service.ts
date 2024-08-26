9198c41eb809689437f0d127c581591d78df27d4
Brayan Acosta <landakkpp@gmail.com>
2024-08-26 15:58:26 -0500

    add login register

--- client/src/app/__services/account.service.ts
+++ client/src/app/__services/account.service.ts
@@ -19,7 +19,19 @@ export class AccountService {
           this.currentUser.set(user);
         }
       })
-    )
+    );
+  }
+
+  register(model:any){
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user;
+      })
+    );
   }
 
   logout(){
