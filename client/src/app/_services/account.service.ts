9b3c7ee8da1016caf1c945ec80f7e46b572f9715
Brayan Acosta <landakkpp@gmail.com>
2024-09-20 17:38:25 -0500

    update urls services

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -13,7 +13,7 @@ export class AccountService {
   currentUser = signal<User | null>(null);
 
   login(model:any){
-    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+    return this.http.post<User>(this.baseUrl + '/account/login', model).pipe(
       map(user => {
         if(user){
           localStorage.setItem('user', JSON.stringify(user));
@@ -24,7 +24,7 @@ export class AccountService {
   }
 
   register(model:any){
-    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+    return this.http.post<User>(this.baseUrl + '/account/register', model).pipe(
       map(user => {
         if(user){
           localStorage.setItem('user', JSON.stringify(user));
