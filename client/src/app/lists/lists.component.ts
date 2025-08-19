854843137dde4fa3e7ed83508cb8eeda1b3440b9
Brayan Landa <landakkpp@gmail.com>
2025-08-19 16:39:45 -0500

    structure code

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
+import { LikesService } from '../_services/likes.service';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ButtonsModule],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit, OnDestroy {
+  private likesServices = inject(LikesService);
+  predicate = 'liked';
+  pageNumber = 1;
+  pageSize = 5;
 
+  ngOnInit() {
+    this.loadLikes();
+  }
+  getTitle(){
+    switch (this.predicate) {
+      case 'liked': return 'Members you like';
+      case 'likedBy': return 'Members who like you';
+      default: return 'Mutual';
+    }
+  }
+  loadLikes() {
+    this.likesServices.getLikes(this.predicate);
+  }
+
+  pageChanged(event: any) {
+    if(this.pageNumber !== event.page){
+      this.pageNumber = event.page;
+      this.loadLikes();
+    }
+  }
+  ngOnDestroy() {
+    this.likesServices;
+  }
 }
